2ad588b07461fb809b75e71079fb3ec4ac0c6da6
Noah Snelson <noah.snelson@protonmail.com>
2019-10-19 13:29:37 -0700

    Refactored connecting into separate function, fixed bug with empty queries

--- src/main.rs
+++ src/main.rs
@@ -5,32 +5,35 @@
 
 use std::net::TcpStream;
 use std::io::*;
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     write_loop().unwrap();
 }
 
 fn write_loop() -> std::io::Result<()> {
-    println!("Enter address (host:port): ");
-    let mut hostname = String::new();
-    std::io::stdin().read_line(&mut hostname).unwrap();
-    let mut stream = TcpStream::connect(hostname.trim())
-        .expect("Could not connect to address.");
-    println!("Connected!");
+    let mut stream = connect();
     let mut query = String::new();
     let buffer_length = 512;
     let mut buffer = [0u8; 512];
     assert_eq!(buffer_length, buffer.len());
     let mut response = Vec::new();
-    loop{
+    loop { // REPL Loop
         print!("> ");
         std::io::stdout().flush()?;
         std::io::stdin().read_line(&mut query).expect("Error reading query");
+        if query.trim().is_empty() {
+            continue;
+        }
+        println!("'{}'", query);
         stream.write(query.as_bytes()).expect("Connection closed.");
-        loop{
+        thread::sleep(Duration::from_millis(1));
+        loop { // Keep reading until message is done being read
             let n_bytes = stream.read(&mut buffer).unwrap();
             response.extend_from_slice(&buffer);
             buffer = [0; 512];
+            println!("Read {} bytes in response: ", n_bytes);
             if n_bytes == 0 || n_bytes < buffer_length {
                 break;
             }
@@ -40,4 +43,15 @@ fn write_loop() -> std::io::Result<()> {
         response.clear();
         query.clear();
     }
+}
+
+fn connect() -> TcpStream {
+    println!("Enter address (host:port): ");
+    //let mut hostname = String::new();
+    //std::io::stdin().read_line(&mut hostname).unwrap();
+    let hostname = String::from("localhost:6969");
+    let stream = TcpStream::connect(hostname.trim())
+        .expect("Could not connect to address.");
+    println!("Connected!");
+    return stream;
 }
