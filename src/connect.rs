cfab4c8ac165e3ab600cd521f483dc4f64d20701
Noah Snelson <noah.snelson@protonmail.com>
2019-10-19 23:15:16 -0700

    Refactored write_loop, still having trouble with non-HTTP sessions

--- src/connect.rs
+++ src/connect.rs
@@ -1,9 +1,40 @@
 use std::net::TcpStream;
+use std::io::*;
 
-pub fn connect(host: &String, port: &String) -> TcpStream {
+fn connect(host: &String, port: &String) -> TcpStream {
     let conn_string = format!("{}:{}", host, port);
     let stream = TcpStream::connect(conn_string.trim())
         .expect("Could not connect to address.");
     println!("Connected to {}", conn_string);
     return stream;
+}
+
+pub fn write_loop(host: &String, port: &String) -> std::io::Result<()> {
+    let mut stream = connect(&host, &port);
+    let mut response = Vec::new();
+    let mut query: String = String::new();
+    loop {
+        read_query(&mut query);
+        query.push('\n');
+        stream.write(query.as_bytes()).expect("Connection closed.");
+        stream.read_to_end(&mut response).unwrap();
+        if response.len() == 0 {
+            println!("Empty response, connection has been closed.");
+            break Ok(());
+        }
+        println!("{}\n", String::from_utf8_lossy(&response));
+        stream.flush()?;
+        response.clear();
+        query.clear();
+    }
+}
+
+fn read_query(mut query: &mut String) {
+    print!("> ");
+    std::io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut query).expect("Error reading query");
+    if query.trim().is_empty() {
+        query.clear();
+        read_query(&mut query);
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,6 @@
  *  Written by Noah Snelson, in the year of Our Lord 2019.
  */
 
-use std::io::*;
-use std::thread;
-use std::time::Duration;
 use std::env;
 mod connect;
 
@@ -17,42 +14,5 @@ fn main() {
     }
     let host: &String = &args[1];
     let port: &String = &args[2];
-    write_loop(&host, &port).unwrap();
+    connect::write_loop(&host, &port).unwrap();
 }
-
-fn write_loop(host: &String, port: &String) -> std::io::Result<()> {
-    let mut stream = connect::connect(&host, &port);
-    let mut query = String::new();
-    let buffer_length = 512;
-    let mut buffer = [0u8; 512];
-    assert_eq!(buffer_length, buffer.len());
-    let mut response = Vec::new();
-    loop { // REPL Loop
-        print!("> ");
-        std::io::stdout().flush()?;
-        std::io::stdin().read_line(&mut query).expect("Error reading query");
-        if query.trim().is_empty() {
-            continue;
-        }
-        query.push('\n');
-        println!("'{}'", query);
-        let w_bytes = stream.write(query.as_bytes())
-            .expect("Connection closed.");
-        println!("Wrote {} bytes", w_bytes);
-        thread::sleep(Duration::from_millis(1));
-        loop { // Keep reading until message is done being read
-            let r_bytes = stream.read(&mut buffer).unwrap();
-            response.extend_from_slice(&buffer);
-            buffer = [0; 512];
-            println!("Read {} bytes in response: ", r_bytes);
-            if r_bytes == 0 /*|| r_bytes < buffer_length*/ {
-                break;
-            }
-        }
-        println!("{}\n", String::from_utf8_lossy(&response));
-        stream.flush()?;
-        response.clear();
-        query.clear();
-    }
-}
-
