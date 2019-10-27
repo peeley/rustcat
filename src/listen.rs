324ea5905524b06573ce58dafd54db746013d174
Noah Snelson <noah.snelson@protonmail.com>
2019-10-26 19:18:18 -0700

    Listen module can now pipe incoming bytes into other processes - need to fix hanging when reading until EOF

--- src/listen.rs
+++ src/listen.rs
@@ -3,7 +3,7 @@ use std::io::{Result, Read, Write};
 use std::process::{Command, Stdio, Child};
 
 pub fn listen_loop(port: &str, command_name: Option<&str>) -> Result<()> {
-    let command: Option<Child> = match command_name {
+    let mut command: Option<Child> = match command_name {
         Some(name) => Some(Command::new(name)
                         .stdin(Stdio::piped())
                         .stdout(Stdio::piped())
@@ -15,24 +15,31 @@ pub fn listen_loop(port: &str, command_name: Option<&str>) -> Result<()> {
     loop {
         for stream in listener.incoming() {
             println!("Incoming connection...");
-            handle_incoming(&mut stream.unwrap(), command);
+            handle_incoming(&mut stream.unwrap(), &mut command);
         }
     }
 }
 
-fn handle_incoming(stream: &mut TcpStream, command: Option<Child>) {
+fn handle_incoming(stream: &mut TcpStream, command: &mut Option<Child>) {
     let mut incoming;
+    let mut output: Vec<u8> = Vec::new();
     loop {
-        incoming = [0; 1024];
+        incoming = [0 as u8; 1024];
         stream.read(&mut incoming)
             .expect("UNABLE TO DECODE INCOMING MESSAGE");
         println!("{}", String::from_utf8_lossy(&incoming));
         match command {
-            Some(c) => c.stdin.unwrap().write_all(&incoming).unwrap(),
+            Some(c) => {
+                c.stdin.as_mut().unwrap().write_all(&incoming).unwrap();
+                // Reading from child process hangs when no end of file
+                c.stdout.as_mut().unwrap().read_to_end(&mut output).unwrap();
+                println!("{}", String::from_utf8_lossy(&output));
+            }
             None => {
-                stream.write(&incoming).unwrap();
-                stream.flush().unwrap();
+                output = incoming.to_vec();
             }
         }
+        stream.write(&output).unwrap();
+        stream.flush().unwrap();
     }
 }
